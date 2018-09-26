528dbc042cf8546c413869d68d7f5e9ac4193dd1
Samuel Stark <popgoestoast@gmail.com>
2018-09-26 17:18:08 +0100

    Made the ComponentSet stuff way cleaner

--- demo/src/free_camera/system.h
+++ demo/src/free_camera/system.h
@@ -3,7 +3,7 @@
 #include "turniprenderer/system.h"
 #include "component.h"
 
-class FreeCameraSystem : public TurnipRenderer::System<TurnipRenderer::ComponentSet<FreeCameraComponent>, TurnipRenderer::ComponentSet<TurnipRenderer::Transform>> {
+class FreeCameraSystem : public TurnipRenderer::System<TurnipRenderer::InputComponents<FreeCameraComponent>, TurnipRenderer::OutputComponents<TurnipRenderer::Transform>> {
 public:
 	FreeCameraSystem(TurnipRenderer::Context& context) : System(context) {}
 	void runOnEntity(TurnipRenderer::Entity* entity, const System::Inputs inputs, const System::Outputs outputs) override;
--- libturniprenderer/include/turniprenderer/system.h
+++ libturniprenderer/include/turniprenderer/system.h
@@ -5,52 +5,19 @@
 #include "entity.h"
 
 namespace TurnipRenderer {
-	template<class... Ts>
+	template<bool Const, class... Ts>
 	class ComponentSet;
-	template<>
-	class ComponentSet<> {
+	template<bool Const>
+	class ComponentSet<Const> {
 	public:
 		template<class SelectedContainerType>
 		static inline bool fillContainer(Entity* entity, SelectedContainerType& container){
 			return true;
 		}
 	};
-	template<class T, class... TRemaining>
-	class ComponentSet<T, TRemaining...> : ComponentSet<TRemaining...> {
+	template<bool Const, class T, class... TRemaining>
+	class ComponentSet<Const, T, TRemaining...> : ComponentSet<Const, TRemaining...> {
 	public:
-		static_assert(!std::is_pointer<T>::value, "ComponentSet cannot contain a pointer type!");
-		static_assert(!std::is_reference<T>::value, "ComponentSet cannot contain a reference type!");
-		static_assert(std::is_base_of<Component, T>::value, "ComponentSet contains a type that isn't a component!");
-		
-		/*static bool ownsAComponent(Entity* entity){
-			for (const auto& component : entity->components){
-				if (dynamic_cast<T*>(component.get()) != nullptr)
-					return true;
-			}
-			return false;
-		}
-		static inline bool entityHasAllComponents(Entity* entity){
-			if (!ownsAComponent(entity)){
-				return false;
-			}
-			return ComponentSet<TRemaining...>::entityHasAllComponents(entity);
-		}*/
-
-		/*template<template<class...> SrcType>
-		static inline SrcType<T, TRemaining...> createContainer(){
-			return SrcType<T, TRemaining...>();
-			}*/
-		// TODO: These could probably use the same thing as a base
-		template<template<class...> class SrcType>
-		using PointerContainerType = SrcType<
-			typename std::add_pointer<typename std::remove_const<T>::type>::type,
-			typename std::add_pointer<typename std::remove_const<TRemaining>::type>::type...
-			>;
-		template<template<class...> class SrcType>
-		using ConstPointerContainerType = SrcType<
-			typename std::add_pointer<typename std::add_const<T>::type>::type,
-			typename std::add_pointer<typename std::add_const<TRemaining>::type>::type...
-			>;
 		// TODO: Move this out
 		template<template<class> class ToApply, class ApplyTo, bool If>
 		struct apply_if;
@@ -62,17 +29,30 @@ namespace TurnipRenderer {
 		struct apply_if<ToApply, ApplyTo, false>{
 			using type = ApplyTo;
 		};
-		// TODO: Find a way to do this without the bool Const
-		template<class SelectedContainerType, bool Const>
+		
+		using ComponentType = typename std::remove_const<T>::type;
+		template<class Type>
+		using PointerType = typename std::add_pointer<typename apply_if<std::add_const, Type, Const>::type>::type;
+		static_assert(is_component_type<ComponentType>::value);
+		
+		template<template<class...> class SrcType>
+		using PointerContainerType = SrcType<
+			PointerType<ComponentType>,
+			PointerType<typename std::remove_const<TRemaining>::type>...
+			>;
+		template<class SelectedContainerType>
 		static inline bool fillContainer(Entity* entity, SelectedContainerType& container){
-			using PointerType = typename std::add_pointer<typename apply_if<std::add_const, T, Const>::type>::type;
-			PointerType componentPtr = entity->getComponent<T>();
+			PointerType<ComponentType> componentPtr = entity->getComponent<T>();
 			if (componentPtr == nullptr)
 				return false;
-			std::get<PointerType>(container) = componentPtr;
-			return ComponentSet<TRemaining...>::fillContainer(entity, container);
+			std::get<PointerType<ComponentType>>(container) = componentPtr;
+			return ComponentSet<Const, TRemaining...>::fillContainer(entity, container);
 		}
 	};
+	template<class... ComponentTypes>
+	class InputComponents : public ComponentSet<true, ComponentTypes...>{};
+	template<class... ComponentTypes>
+	class OutputComponents : public ComponentSet<false, ComponentTypes...>{};
 
 	class SystemBase : protected ContextAware {
 	public:
@@ -85,20 +65,18 @@ namespace TurnipRenderer {
 	class System : public SystemBase {
 		using SystemBase::SystemBase;
 		
-		static_assert(std::is_base_of<ComponentSet<>, InputStorage>::value);
-		static_assert(std::is_base_of<ComponentSet<>, OutputStorage>::value);
-		static_assert(!std::is_same<ComponentSet<>, OutputStorage>::value, "A system must output something!");
+		static_assert(std::is_base_of<ComponentSet<true>, InputStorage>::value);
+		static_assert(std::is_base_of<ComponentSet<false>, OutputStorage>::value);
+		static_assert(!std::is_same<ComponentSet<false>, OutputStorage>::value, "A system must output something!");
 	public:
-		using Inputs = typename InputStorage::template ConstPointerContainerType<ComponentContainerType>;
+		using Inputs = typename InputStorage::template PointerContainerType<ComponentContainerType>;
 		using Outputs = typename OutputStorage::template PointerContainerType<ComponentContainerType>;
-		//		using Test = typename Inputs::A;
-		//using Test2 = typename Outputs::A;
 
 		bool runOnEntityIfValid(Entity* entity) override {
 			Inputs inputs;
 			Outputs outputs;
-			if (!InputStorage::template fillContainer<Inputs, true>(entity, inputs)) return false;
-			if (!OutputStorage::template fillContainer<Outputs, false>(entity, outputs)) return false;
+			if (!InputStorage::template fillContainer<Inputs>(entity, inputs)) return false;
+			if (!OutputStorage::template fillContainer<Outputs>(entity, outputs)) return false;
 			runOnEntity(entity, inputs, outputs);
 			return true;
 		}
