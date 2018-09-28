9c36e667a5846375d34a609417dcd7f09d9b6846
watergun127 <samuels@ccss.co.uk>
2018-09-28 07:08:13 -0300

    Fixes for the generate_sources scripts

--- demo/generate_sources.sh
+++ demo/generate_sources.sh
@@ -4,6 +4,7 @@ cd "$(dirname $0)"
 header_files=$(find -name "*.h" -o -name "*.hpp") # Doesn't produce a list, produces a string
 c_files=$(find -name "*.c" -o -name "*.cpp")
 final_content=$(printf "set(TR_DEMO_HEADER_FILES $header_files)\nset(TR_DEMO_SRC_FILES $c_files)")
+touch CMakeSources.txt
 if [[ $(< CMakeSources.txt) != "$final_content" ]]; then
 	echo "$final_content" > CMakeSources.txt
 else
--- libturniprenderer/generate_sources.sh
+++ libturniprenderer/generate_sources.sh
@@ -7,6 +7,7 @@ PRIVATE_INCLUDE_DIR=include/turniprenderer/
 header_files=$(find src/ ${PRIVATE_INCLUDE_DIR} -name "*.h" -o -name "*.hpp") # Doesn't produce a list, produces a string
 c_files=$(find src/ -name "*.c" -o -name "*.cpp")
 final_content=$(printf "set(TR_PUBLIC_INCLUDE_DIR \${CMAKE_CURRENT_SOURCE_DIR}/$PUBLIC_INCLUDE_DIR CACHE STRING \"Include Directory for TurnipRenderer\")\nset(TR_PRIVATE_INCLUDE_DIR \${CMAKE_CURRENT_SOURCE_DIR}/$PRIVATE_INCLUDE_DIR)\nset(TR_HEADER_FILES $header_files)\nset(TR_SRC_FILES $c_files)")
+touch CMakeSources.txt
 if [[ $(< CMakeSources.txt) != "$final_content" ]]; then
 	echo "$final_content" > CMakeSources.txt
 else
