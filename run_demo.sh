d49a6eb03cf19f8c12ce25c17b5ee3c9a531242b
Samuel Stark <popgoestoast@gmail.com>
2018-10-07 13:45:16 +0100

    Made the run_demo script better

--- run_demo.sh
+++ run_demo.sh
@@ -1,6 +1,13 @@
 cd ./build/demo/
-if [ $1 == "debug" ]; then
-	gdb ./demo
+if [ $# -ge 1 ]; then
+	 if [ $1 == "debug" ]; then
+		 gdb ./demo
+	 elif [ $1 == "valgrind" ]; then
+		 valgrind ./demo
+	 else
+		 echo "Invalid argument: must be 'debug' or 'valgrind'"
+		 exit 1
+	 fi
 else
 	./demo
 fi
